b666461280a77e4a23512eeb95d7edecd97543f2
shigemasa yuki <yushin1301@gmail.com>
2021-11-01 17:39:28 +0900

    add common componentes

---
+++ src/components/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  ivent: [
+    {
+      title: "Test",
+      time: 2021 / 10,
+      describe: "test test test",
+      url: "https://twitter.com/gegesonyushin",
+      twitter: "https://twitter.com/gegesonyushin",
+    },
+    {
+      title: "Test",
+      time: 2021 / 10,
+      describe: "test test test",
+      url: "https://twitter.com/gegesonyushin",
+      twitter: "https://twitter.com/gegesonyushin",
+    },
+    {
+      title: "Test",
+      time: 2021 / 10,
+      describe: "test test test",
+      url: "https://twitter.com/gegesonyushin",
+      twitter: "https://twitter.com/gegesonyushin",
+    },
+  ],
+};
