ef4662b88a59e78638d28d4d8435c48845ee9580
shigemasa yuki <yushin1301@gmail.com>
2021-11-01 23:58:58 +0900

    [add] Header

---
+++ src/components/header.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import MenuBookSharpIcon from "@mui/icons-material/MenuBookSharp";
+import DateRangeIcon from "@mui/icons-material/DateRange";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import "../css/header.scss";
+import SvgIcon from "@mui/material/SvgIcon";
+
+const navList = [
+  {
+    title: "Timeline",
+    url: "/timeline",
+    icon: "M23 8c0 1.1-.9 2-2 2-.18 0-.35-.02-.51-.07l-3.56 3.55c.05.16.07.34.07.52 0 1.1-.9 2-2 2s-2-.9-2-2c0-.18.02-.36.07-.52l-2.55-2.55c-.16.05-.34.07-.52.07s-.36-.02-.52-.07l-4.55 4.56c.05.16.07.33.07.51 0 1.1-.9 2-2 2s-2-.9-2-2 .9-2 2-2c.18 0 .35.02.51.07l4.56-4.55C8.02 9.36 8 9.18 8 9c0-1.1.9-2 2-2s2 .9 2 2c0 .18-.02.36-.07.52l2.55 2.55c.16-.05.34-.07.52-.07s.36.02.52.07l3.55-3.56C19.02 8.35 19 8.18 19 8c0-1.1.9-2 2-2s2 .9 2 2z",
+  },
+  {
+    title: "Bookshelf",
+    url: "/book",
+    icon: "M21 5c-1.11-.35-2.33-.5-3.5-.5-1.95 0-4.05.4-5.5 1.5-1.45-1.1-3.55-1.5-5.5-1.5S2.45 4.9 1 6v14.65c0 .25.25.5.5.5.1 0 .15-.05.25-.05C3.1 20.45 5.05 20 6.5 20c1.95 0 4.05.4 5.5 1.5 1.35-.85 3.8-1.5 5.5-1.5 1.65 0 3.35.3 4.75 1.05.1.05.15.05.25.05.25 0 .5-.25.5-.5V6c-.6-.45-1.25-.75-2-1zm0 13.5c-1.1-.35-2.3-.5-3.5-.5-1.7 0-4.15.65-5.5 1.5V8c1.35-.85 3.8-1.5 5.5-1.5 1.2 0 2.4.15 3.5.5v11.5z",
+  },
+  {
+    title: "Github",
+    url: "https://github.com/gegeson",
+    icon: "M12 1.27a11 11 0 00-3.48 21.46c.55.09.73-.28.73-.55v-1.84c-3.03.64-3.67-1.46-3.67-1.46-.55-1.29-1.28-1.65-1.28-1.65-.92-.65.1-.65.1-.65 1.1 0 1.73 1.1 1.73 1.1.92 1.65 2.57 1.2 3.21.92a2 2 0 01.64-1.47c-2.47-.27-5.04-1.19-5.04-5.5 0-1.1.46-2.1 1.2-2.84a3.76 3.76 0 010-2.93s.91-.28 3.11 1.1c1.8-.49 3.7-.49 5.5 0 2.1-1.38 3.02-1.1 3.02-1.1a3.76 3.76 0 010 2.93c.83.74 1.2 1.74 1.2 2.94 0 4.21-2.57 5.13-5.04 5.4.45.37.82.92.82 2.02v3.03c0 .27.1.64.73.55A11 11 0 0012 1.27",
+  },
+];
+
+export default function Header() {
+  return (
+    <Nav className="justify-content-end">
+      {navList.map(({ title, url, icon }, i) => (
+        <Nav.Item>
+          <Nav.Link key={i} href={url} className="navlink">
+            <SvgIcon>
+              <path d={icon} />
+            </SvgIcon>
+            {title}
+          </Nav.Link>
+        </Nav.Item>
+      ))}
+    </Nav>
+  );
+}
